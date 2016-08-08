bfe9091f2b0fdb3fb70ca4ea6989be940b9d1416
Amit Upadhyay <youarefreak1@gmail.com>
2016-08-08 19:28:20 +0530

    Create 3.cpp

---
+++ String And Array/3.cpp
@@ -0,0 +1,27 @@
+/*
+check if one string is permutation of other or not.
+*/
+#include<iostream>
+#include<algorithm>// for std::sort()
+bool isPermutation(std::string str1, std::string str2)
+{
+    std::sort(str1.begin(), str1.end());
+    std::sort(str2.begin(), str2.end());
+
+    if(str1 == str2)
+        return true;
+    else
+        return false;
+}
+int main()
+{
+    std::string str1, str2;
+    std::getline(std::cin, str1);
+    std::getline(std::cin, str2);
+    if (isPermutation(str1, str2))
+        std::cout<<"Permutation";
+    else
+        std::cout<<"Not Permutation";
+    std::cout<<std::endl;
+    return 0;
+}
