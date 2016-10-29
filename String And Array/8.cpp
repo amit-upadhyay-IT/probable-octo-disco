9c20ebef2585d3cf04eaf3597c8725fb49410f4c
Amit Upadhyay <youarefreak1@gmail.com>
2016-10-29 13:00:13 +0530

    Update 8.cpp

--- String And Array/8.cpp
+++ String And Array/8.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 
+bool isSubstring(std::string, std::string);
+bool isRotation(std::string, std::string);
+
 int main()
 {
     std::string s1, s2;
@@ -7,18 +10,31 @@ int main()
     std::getline(std::cin, s1);
     std::cout<<"Enter second string \n";
     std::getline(std::cin, s2);
+    isRotation(s1, s2)?std::cout<<"YES":std::cout<<"NO";
+}
 
+bool isRotation(std::string s1, std::string s2)
+{
+    std::string s1s1 = s1+s1;
+    return (isSubstring(s1s1, s2));
 }
 
 bool isSubstring(std::string s1, std::string s2)
 {
     int counter = 0;
     int i, j;
-    for (i = 0, j = 0; i < s1.length(); ++i)
+    for (i = 0, j = 0; i < s1.length() && j < s2.length(); ++i)
     {
          if (s2[j] == s1[i])
          {
-
+             j++;
+             counter++;
+         }
+         else
+         {
+             j = 0;
+             counter = 0;
          }
     }
+    return (counter == s2.length());
 }
