f35ca5b5385052f74c5b5c9ff7ca6b112be74f98
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-02-11 15:03:19 +0530

    smaller length way using list comprehensions

--- python/matrix_stuffs/05inp_disp.py
+++ python/matrix_stuffs/05inp_disp.py
@@ -1,1 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/pytho
+row = input("Enter number of rows: ")
+cols = input("Enter number of columns: ")
+a = [[input("Enter element : ") for i in range(row)] for j in range(cols)]
+for i in range(row):
+    print a[i]
