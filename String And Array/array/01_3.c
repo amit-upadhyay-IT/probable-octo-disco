65c24fc2c07a15fece720db8060ba0cc5bc72d55
Amit Upadhyay <youarefreak1@gmail.com>
2017-06-22 13:59:28 +0530

    Create 01_3.c

---
+++ String And Array/array/01_3.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+// NOTE: array input has to be sorted for correct output.
+void check_pair(int *, int, int);
+
+int main()
+{
+    int arr[50], i, n;
+    scanf("%d", &n);
+    for (i = 0; i < n; ++i)
+        scanf("%d", &arr[i]);
+    int x;
+    scanf("%d", &x);
+    check_pair(arr, n, x);
+    return 0;
+}
+
+void check_pair(int *arr, int n, int x)
+{
+    // take firstpos and lastpos and store element value there.
+    int first_pos = 0;
+    int last_pos = n-1;
+    while (first_pos <= last_pos)
+    {
+        if (arr[first_pos]+arr[last_pos] < x)
+            first_pos = first_pos+1;
+        else if (arr[first_pos] + arr[last_pos] > x)
+            last_pos = last_pos - 1;
+        else
+        {
+            printf("(%d , %d)\n", arr[first_pos], arr[last_pos]);
+            first_pos += 1;
+            last_pos -= 1;
+        }
+    }
+}
