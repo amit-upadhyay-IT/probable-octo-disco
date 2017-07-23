3eea4ccacabcb4a1284acb2490a67029feb67a5b
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-07-23 19:06:27 +0530

    8_1

---
+++ SnA/array/08_1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void show_triplets(int *arr, int n, int x)
+{
+    int i, j, k;
+    for (i = 0; i < n; ++i)
+        for (j = i+1; j < n; ++j)
+            for (k = j+1; k < n; ++k)
+                if (arr[i] + arr[j] + arr[k] == x)
+                    printf("(%d, %d, %d)", arr[i], arr[j], arr[k]);
+}
+
+int main()
+{
+    int n, *arr, i, x;
+    scanf("%d", &n);
+    arr = (int*)malloc(n*sizeof(int));
+    for (i = 0; i < n; ++i)
+        scanf("%d", &arr[i]);
+    scanf("%d", &x);
+    show_triplets(arr, n, x);
+    return 0;
+}
