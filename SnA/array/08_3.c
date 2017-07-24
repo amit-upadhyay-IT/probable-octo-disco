928dc6dbc1db3d29bfc5f827e84a4e5b52c127bc
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-07-24 22:05:33 +0530

    updated 8_3

--- SnA/array/08_3.c
+++ SnA/array/08_3.c
@@ -21,9 +21,33 @@ int numcmp(const void *a, const void *b)
     return (*(int*)a - *(int*)b);
 }
 
+int check_for_pair(int *arr, int n, int start, int x)
+{
+    int first_pos = start;
+    int last_pos = n-1;
+    int i;
+    while (first_pos < last_pos)
+    {
+        int sum = arr[first_pos] + arr[last_pos] + arr[start-1];
+        if (sum < x)
+            first_pos++;
+        else if (sum > x)
+            last_pos--;
+        else
+        {
+            printf("(%d, %d, %d)\n", arr[start-1], arr[first_pos], arr[last_pos]);
+            first_pos++; last_pos--;
+        }
+    }
+}
+
 void find_triplets(int *arr, int n, int x)
 {
     int i;
+    for (i = 1; i < n; ++i)
+    {
+        check_for_pair(arr, n, i, x);
+    }
 }
 
 int main()
@@ -34,4 +58,6 @@ int main()
     input_array(arr, n);
     scanf("%d", &x);
     qsort(arr, n, sizeof(int), numcmp);
+    find_triplets(arr, n, x);
+    return 0;
 }
