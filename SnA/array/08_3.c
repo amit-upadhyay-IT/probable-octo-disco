ce9915e54c9ed1ffedcec783472f37fdd7a218ea
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-07-24 20:39:47 +0530

    8_3

--- SnA/array/08_3.c
+++ SnA/array/08_3.c
@@ -1,2 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+void input_array(int *arr, int n)
+{
+    int i;
+    for (i = 0; i< n; ++i)
+        scanf("%d", &arr[i]);
+}
+
+void print_array(int *arr, int n)
+{
+    int i;
+    for (i = 0; i< n; ++i)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int numcmp(const void *a, const void *b)
+{
+    return (*(int*)a - *(int*)b);
+}
+
+void find_triplets(int *arr, int n, int x)
+{
+    int i;
+}
+
+int main()
+{
+    int i, n, *arr, x;
+    scanf("%d", &n);
+    arr = (int *)malloc(n * sizeof(int));
+    input_array(arr, n);
+    scanf("%d", &x);
+    qsort(arr, n, sizeof(int), numcmp);
+}
