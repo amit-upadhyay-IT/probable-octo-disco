eb340b1a4258afc24585ee1dc5612a30d7b41012
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-07-30 06:28:00 +0530

    ..

--- ll/intro.c
+++ ll/intro.c
@@ -12,13 +12,26 @@ struct node* get_new_node()
     return (struct node*)malloc(sizeof(struct node));
 }
 
-void insert_at_beginning(struct node* head, int data)
+struct node* insert_at_beginning(struct node* head, int data)
 {
     struct node* new_node = get_new_node();
     new_node->data = data;
     new_node->link = head;
     head = new_node;
-    printf("\n%x\n", head);
+    //printf("\n%x\n", head);
+    return head;
+}
+
+void insert_at_end(struct node* head, int data)
+{
+    struct node* temp;
+    temp = head;
+    while (temp->link)
+        temp = temp->link;
+    struct node* new_node = get_new_node();
+    temp->link = new_node;
+    new_node->data = data;
+    new_node->link = NULL;
 }
 
 void traverse_ll(struct node* head)
@@ -36,20 +49,23 @@ int main()
     int i;
     struct node* head;
     int ch, data;
-    head = (struct node*)malloc(sizeof(struct node));
-    head ->data = 4;
     do
     {
-        printf("\nEnter your choice : \n1.Insert at beginning\n2.Traverse\n");
+        printf("\nEnter your choice : \n1.Insert at beginning\n2.Insert at end\n3.Traverse\n");
         scanf("%d", &ch);
         switch(ch)
         {
             case 1:
                 printf("\nEnter the data\n");
                 scanf("%d", &data);
-                insert_at_beginning(head, data);
+                head = insert_at_beginning(head, data);
                 break;
             case 2:
+                printf("\nEnter the data\n");
+                scanf("%d", &data);
+                insert_at_end(head, data);
+                break;
+            case 3:
                 traverse_ll(head);
                 break;
         }
