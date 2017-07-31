6a043c62f2eeda173492c7ff3dccdcbd79912e03
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-07-31 12:38:55 +0530

    mid node updated

--- ll/02_1.c
+++ ll/02_1.c
@@ -28,6 +28,22 @@ struct node* insert_end(struct node* head, int data)
     return head;
 }
 
+int nodes_in_ll(struct node* head)
+{
+    int cnt = 0;
+    struct node* temp = head;
+    if (!head)
+        return 0;
+    else if (!head->next)
+        return 1;
+    while (temp->next)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+    return cnt;
+}
+
 void print_ll(struct node* head)
 {
     if (head)
@@ -36,6 +52,21 @@ void print_ll(struct node* head)
         print_ll(head->next);
     }
 }
+
+struct node* get_mid_node(struct node* head, int mid)
+{
+    if (head == NULL)
+        return NULL;
+    struct node* temp = head;
+
+    while (mid > 0)
+    {
+         mid--;
+         temp = temp->next;
+    }
+    return temp;
+}
+
 
 int main()
 {
@@ -51,5 +82,10 @@ int main()
     print_ll(head);
     printf("\n");
 
+    int cnt = nodes_in_ll(head);
+    printf("\n Number of  nodes : %d \n", cnt+1);
+
+    struct node* mid_node = get_mid_node(head, cnt/2);
+    printf("\n The moid node is %d \n", mid_node->data);
     return 0;
 }
