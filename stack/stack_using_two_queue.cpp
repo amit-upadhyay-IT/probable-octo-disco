09114df526ab926b192ae5f5370cbabe7fe33ecf
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-08-10 20:14:43 +0530

    stacks using queues

---
+++ stack/stack_using_two_queue.cpp
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<iostream>
+#include<queue>
+
+void push(std::queue<int> q1, std::queue<int> q2, int item)
+{
+    // insert in any of the empty queue
+    if (q1.empty())
+    {
+        q1.push(item);
+        if (q2.empty())
+        {
+            // do nothing
+        }
+        else
+        {
+            // dequeue elements from q2 and enqueue them in q1
+            while (!q2.empty())
+            {
+                int _val = q2.front();
+                q2.pop();
+                q1.push(_val);
+            }
+        }
+    }
+    else if (q2.empty())
+    {
+        // q1 is not empty and q2 is empty, so insert into q2
+        q2.push(item);
+        while (!q1.empty())
+        {
+            int _val = q1.front();
+            q1.pop();
+            q2.push(_val);
+        }
+    }
+}
+
+int pop(std::queue<int> q1, std::queue<int> q2)
+{
+    if (!q1.empty())
+    {
+        int _val = q1.front();
+        q1.pop();
+        return _val;
+    }
+    else
+    {
+        // q2 is not empty
+        int _val = q2.front();
+        q2.pop();
+        return _val;
+    }
+}
+
+int main()
+{
+    std::queue<int> q1, q2;
+
+    return 0;
+}
