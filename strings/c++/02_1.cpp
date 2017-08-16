f992dc95111750d70157af7407622fcc172203d8
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-08-16 20:25:07 +0530

    unique set

---
+++ strings/c++/02_1.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<cstring>
+
+bool search_in_arr(char *unique_set, char c)
+{
+    for (int i = 0; i < strlen(unique_set); ++i)
+    {
+        if (unique_set[i] == c)
+            return true;
+    }
+    return false;
+}
+
+void insert_in_set(char *unique_set, char ch)
+{
+    int len = strlen(unique_set);
+    unique_set[len] = ch;
+}
+
+int check_unique(std::string str, char *unique_set)
+{
+    for (int i = 0; i< str.length(); ++i)
+    {
+        if (!search_in_arr(unique_set, str[i]))
+        {
+            insert_in_set(unique_set, str[i]);
+        }
+    }
+}
+
+void print_unique_arr(char * unique_set)
+{
+    for (int i = 0; i < strlen(unique_set); ++i)
+    {
+        std::cout<<unique_set[i];
+    }
+    std::cout<<std::endl;
+}
+
+int main()
+{
+    std::string str;
+    char unique_set[256] = {'\0'};
+
+    std::getline(std::cin, str);
+
+    check_unique(str, unique_set);
+    print_unique_arr(unique_set);
+    return 0;
+}
