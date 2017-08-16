7e6bedd259a5b4083f1acb10d99c6036178c4928
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-08-16 23:13:21 +0530

    2-2 in cpp

---
+++ strings/c++/02_2.cpp
@@ -0,0 +1,38 @@
+/*
+ * printing only unique characters in a string
+ * */
+
+#include<iostream>
+
+void hashing(std::string str, int *char_set)
+{
+    for (int i = 0; i < str.length(); ++i)
+    {
+        char_set[str[i]]++;
+    }
+}
+
+void print_string(std::string str, int *char_set)
+{
+    for (int i = 0; i < str.length(); ++i)
+    {
+        if (char_set[str[i]])
+        {
+            std::cout<<str[i];
+            char_set[str[i]] = 0;
+        }
+    }
+    std::cout<<std::endl;
+}
+
+int main()
+{
+    int char_set[256] = {0};
+    std::string str;
+
+    std::getline(std::cin, str);
+
+    hashing(str, char_set);
+    print_string(str, char_set);
+    return 0;
+}
