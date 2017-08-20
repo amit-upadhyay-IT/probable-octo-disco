e07d497bd8777eda411b9fdca2560430b039eb58
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-08-20 19:31:54 +0530

    approach2 for getting lca

---
+++ trees/c++/06_2.cpp
@@ -0,0 +1,77 @@
+/*
+ * find lca
+ * tree used is BST
+ * approach used:
+ * search for the val1 and val2 simultaneously and in the process of searching
+ * note the pointer where we are get split into two different directions.
+ * */
+#include<iostream>
+#include<queue>
+
+struct node
+{
+    int data;
+    node* left, *right;
+};
+
+node* get_new_node(int data)
+{
+    node* new_node = new node();
+    new_node->data = data;
+    new_node->left = new_node->right = NULL;
+    return new_node;
+}
+
+node* insert(node* root, int data)
+{
+    // case 1, when root is empty
+    if (!root)
+        root = get_new_node(data);
+
+    // case 2, when data is less
+    else if (root->data > data)
+        root->left = insert(root->left, data);
+
+    // case 3, when data is greater than parent
+    else if (root->data < data)
+        root->right = insert(root->right, data);
+
+    return root;
+}
+
+void print_array(node* arr[], int k)
+{
+    for (int i = 0; i < k; ++i)
+    {
+        std::cout<<arr[i]->data<<" ";
+    }
+    std::cout<<std::endl;
+}
+
+node* get_lca(node* root, int val1, int val2)
+{
+    node* arr1[100], *arr2[100];
+    int size1 = get_array(root, val1, arr1);
+    int size2 = get_array(root, val2, arr2);
+    return compare_arrays(arr1, arr2);
+}
+
+int main ()
+{
+    node* root = NULL;
+    root = insert(root, 100);
+    root = insert(root, 70);
+    root = insert(root, 75);
+    root = insert(root, 130);
+    root = insert(root, 115);
+    root = insert(root, 150);
+    root = insert(root, 60);
+    root = insert(root, 65);
+
+    node* lca_node = get_lca(root, 65, 150);
+    std::cout<<lca_node->data;
+
+    std::cout<<std::endl;
+    return 0;
+}
+
