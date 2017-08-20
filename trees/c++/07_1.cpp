590bfbc6d0cc222ec117bef190fc71c935289d89
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-08-20 22:33:36 +0530

    updated

--- trees/c++/07_1.cpp
+++ trees/c++/07_1.cpp
@@ -7,7 +7,8 @@
  * compare two arrays and see the last common node in the path
  * */
 #include<iostream>
-#include<queue>
+#include<stack>
+#include<stdlib.h>
 
 struct node
 {
@@ -40,6 +41,51 @@ node* insert(node* root, int data)
     return root;
 }
 
+int get_array(node* root, node* arr[], int val)
+{
+    int k = 0;
+
+    if (root)
+    {
+        std::stack<node*> s;
+        s.push(root);
+
+        while (!s.empty())
+        {
+            node* temp = s.top(); s.pop();
+            arr[k++] = temp;
+            if (temp->data == val)
+                break;
+            if (temp->right)
+                s.push(temp->right);
+            if (temp->left)
+                s.push(temp->left);
+
+        }
+    }
+    return k;
+}
+
+void print_array(node** arr, int k)
+{
+    for  (int i = 0; i < k; ++i)
+    {
+        std::cout<<arr[i]->data<<" ";
+    }
+    std::cout<<std::endl;
+}
+
+node* get_lca(node* root, int val1, int val2)
+{
+    node** arr1 = (node**)malloc(sizeof(node*)*100);
+    node** arr2 = (node**)malloc(sizeof(node*)*100);
+    int size1 = get_array(root, arr1, val1);
+    int size2 = get_array(root, arr2, val2);
+
+    print_array(arr1, size1);
+    print_array(arr2, size2);
+}
+
 
 int main ()
 {
@@ -53,6 +99,9 @@ int main ()
     root = insert(root, 60);
     root = insert(root, 65);
 
+    std::cout<<std::endl;
+
+    get_lca(root, 65, 75);
     std::cout<<std::endl;
     return 0;
 }
