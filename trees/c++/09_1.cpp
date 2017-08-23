e5a3e0a457aa75df774b13804b9ab394d207e01e
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-08-23 10:59:43 +0530

    solution not passing all test cases 9-1

--- trees/c++/09_1.cpp
+++ trees/c++/09_1.cpp
@@ -1,3 +1,6 @@
+/*
+ * find the diameter of a given binary tree.
+ * */
 #include<iostream>
 
 struct node
@@ -25,6 +28,29 @@ node* insert(node* root, int data)
     return root;
 }
 
+int find_height(node* root)
+{
+    if (!root)
+        return 0;
+    if (!root->left && !root->right)// for leaf
+        return 0;
+    return (1+std::max(find_height(root->left), find_height(root->right)));
+}
+
+int max_diameter = 0;
+
+void set_diameter(node* root)
+{
+    if (!root)
+        return ;
+    set_diameter(root->left);
+    int l = find_height(root->left);
+    int r = find_height(root->right);
+    if (l+r+1 > max_diameter)
+        max_diameter = l+r+1+1;// 1 is added to count the parent node in that path, and another 1 is added because when we are calculating height of LST and RST then both of them are starting from 0, but while counting the diameter we need to count the number of nodes, so we add one.
+    set_diameter(root->right);
+}
+
 void inorder(node* root)
 {
     if (root)
@@ -39,15 +65,45 @@ int main()
 {
     node* root = NULL;
 
+    /*
     root = insert(root, 100);
-    root = insert(root, 50);
     root = insert(root, 70);
-    root = insert(root, 80);
     root = insert(root, 110);
+    root = insert(root, 50);
+    root = insert(root, 75);
+    root = insert(root, 78);
+    root = insert(root, 80);
+    root = insert(root, 82);
+    root = insert(root, 45);
+    root = insert(root, 40);
+    root = insert(root, 57);
+    */
+
+    /*
+    root = insert(root, 10);
+    root = insert(root, 5);
+    root = insert(root, 15);
+    root = insert(root, 16);
+    root = insert(root, 4);
+    */
+
+    /*
+    root = insert(root, 2);
+    root = insert(root, 1);
+    root = insert(root, 3);
+    */
+
+    root = insert(root, 10);
+    root = insert(root, 5);
+    root = insert(root, 30);
+    root = insert(root, 4);
+    root = insert(root, 6);
 
     //inorder(root);
     std::cout<<std::endl;
 
+    set_diameter(root);
+    std::cout<<"Diameter = "<<max_diameter;
     std::cout<<std::endl;
     return 0;
 }
