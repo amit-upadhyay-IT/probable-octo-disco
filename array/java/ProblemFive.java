18ed2e30d19a71dd4f52edb8d04a568d3339105b
amit-upadhyay-it <youarefreak1@gmail.com>
2017-08-26 15:35:51 +0530

    oddly occurred

---
+++ array/java/ProblemFive.java
@@ -0,0 +1,22 @@
+public class ProblemFive {
+
+	public int getOddlyOccurredElement(int []arr, int n)
+	{
+		int val = 0;
+		for (int i = 0; i < n; ++i)
+		{
+			val = val ^ arr[i];
+		}
+		return val;// doesn't work when we have 0 occurring odd number of times.
+	}
+	
+	public static void main(String []args)
+	{
+		int arr[] = {10, 20, 30, 40, 30, 20, 10};
+		
+		int val = new ProblemFive().getOddlyOccurredElement(arr, arr.length);
+		
+		System.out.println("Oddly occurred Element is "+val);
+	}
+}
+
