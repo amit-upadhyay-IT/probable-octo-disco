b2481acdfe323ffa60bcc9eed7bca3e8a6b007ac
amit-upadhyay-it <youarefreak1@gmail.com>
2017-08-28 19:01:18 +0530

    updated and works fine for all test cases

--- array/c/13_2.c
+++ array/c/13_2.c
@@ -13,11 +13,18 @@ void printSubArray(int *arr, int firstIndex, int secondIndex)
 void getSubArray(int *arr, int n, int x)
 {
     int i, j, sum = 0;
+    int i_incr = 0, j_incr = 0;
     i = 0, j = 0;// i and j are leftpointer and rightpointer respectively.
     int flag = 0;
     while (i < n && j < n)
     {
-        sum = arr[i] + arr[j];
+        if (i == 0 && j == 0)
+            sum = arr[i];
+        if (i_incr)
+            sum = sum - arr[i-1];// deleting the value which is not the part of subarray now
+        else if (j_incr)
+            sum = sum + arr[j];
+
         if (sum == x)
         {
             printSubArray(arr, i, j);
@@ -25,9 +32,17 @@ void getSubArray(int *arr, int n, int x)
             break;
         }
         if (sum < x)
+        {
+            j_incr = 1;
             j++;// incrementing rightpointer because we need to add few more elements in the set
+            i_incr = 0;
+        }
         else
+        {
+            i_incr = 1;
             i++;
+            j_incr = 0;
+        }
     }
     if (!flag)
         printf("no sub array with sum %d\n", x);
