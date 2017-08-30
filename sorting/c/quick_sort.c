67248fff6e2193896150af36a9677577290be41a
amit-upadhyay-it <youarefreak1@gmail.com>
2017-08-30 17:43:34 +0530

    updated

--- sorting/c/quick_sort.c
+++ sorting/c/quick_sort.c
@@ -19,17 +19,18 @@ void printArray(int *arr, int n)
 int getPartiotionIndex(int *arr, int firstIndex, int lastIndex)
 {
     int i;
-    int pivot = arr[lastIndex];
+    int pivot = arr[lastIndex];// initializing the pivot element with end element in the array
     int partitionIndex = firstIndex;
     for (i = firstIndex; i < lastIndex; ++i)
     {
-        if (arr[i] <= pivot)
+        if (arr[i] <= pivot)// if the element at beginning seems to be smaller than pivot then we swap elements at i and partitionIndex.
+            //but why? because i is always ahead or equal to partitionIndex and we want no smaller elements than pivot should be after partitionIndex thus we swap these
         {
             swap(arr, i, partitionIndex);
             partitionIndex++;
         }
     }
-    swap(arr, lastIndex, partitionIndex);
+    swap(arr, lastIndex, partitionIndex);// in the end we just put the pivot in its right position(which is partitionIndex).
     return partitionIndex;
 }
 
