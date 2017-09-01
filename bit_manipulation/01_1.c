bd16965e51c5d9d6bd86741239d5c0662dd61058
amit-upadhyay-it <youarefreak1@gmail.com>
2017-09-01 23:47:32 +0530

    kth bit set or unset

--- bit_manipulation/01_1.c
+++ bit_manipulation/01_1.c
@@ -2,9 +2,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int checkKthBit(int n, int k)
+{
+    int otherNum = 1<<k;
+    return n & otherNum;
+}
+
 int main()
 {
-    int n;
+    int n, k;
     printf("\nEnter n\n");
     scanf("%d", &n);
+    printf("Enter k\n");
+    scanf("%d", &k);
+    if (checkKthBit(n, k))
+        printf("\n%d bit is set", k);
+    else
+        printf("\n%d bit is unset", k);
+    printf("\n");
+    return 0;
 }
