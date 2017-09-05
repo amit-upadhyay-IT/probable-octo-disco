04c3bee9729eb51f18fc7ae93ab0f72b3c4c261f
amit-upadhyay-it <youarefreak1@gmail.com>
2017-09-05 17:47:20 +0530

    rotate left

---
+++ array/c/19_1.c
@@ -0,0 +1,46 @@
+/*
+ * rotate elements of array by d
+ * approach 1 : left rotate
+ * */
+#include<stdio.h>
+#include<stdlib.h>
+
+void rotateLeft(int *arr, int n, int d)
+{
+    int *temp = malloc(sizeof(int) * d);
+    int i;
+    for (i = 0; i < d; ++i)
+        temp[i] = arr[i];
+
+    // shifting the elements
+    for(i = d; i < n; ++i)
+        arr[i-d] = arr[i];
+
+    // copying the last d elements from temp
+    for (i = 0; i < d; ++i)
+        arr[n-d+i] = temp[i];
+}
+
+void printArray(int *arr, int n)
+{
+    int i;
+    for (i = 0; i < n; ++i)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int d = 2;
+    //scanf("%d", &d);
+
+    rotateLeft(arr, n, d);
+
+    printArray(arr, n);
+    return 0;
+}
+
+// Time is n^2
