cf1f1dff53534c7c155e03e298c6273805519052
amit-upadhyay-it <youarefreak1@gmail.com>
2017-09-06 23:26:00 +0530

    possible triangles

--- array/c/21_2.c
+++ array/c/21_2.c
@@ -47,6 +47,26 @@ void printArray(int *arr, int n)
     printf("\n");
 }
 
+int getTriangleCount(int *arr, int n)
+{
+    int i, j, k, cnt = 0;
+    i = arr[0];
+
+    for (i = 0; i < n-2; ++i)
+    {
+        // run a loop which will increment j if required
+        for (j = i + 1; j < n-1; ++j)
+        {
+            k = j+1;
+            // repeat this loop until you found the sum of ith and jth element is less
+            while (k < n && arr[i] + arr[j] > arr[k])
+                k++;
+            cnt = cnt + k-j-1;
+        }
+    }
+    return cnt;
+}
+
 int main()
 {
     int arr[] = {6, 7, 8, 10, 12, 14, 50};
@@ -54,7 +74,8 @@ int main()
 
     quickSort(arr, 0, size-1);
 
-    printArray(arr, size);
+    int count = getTriangleCount(arr, size);
+    printf("\nThe number possible triangles are %d", count);
 
     printf("\n");
     return 0;
