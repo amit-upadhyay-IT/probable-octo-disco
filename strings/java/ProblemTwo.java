7fd53f2e47492b683d2fe3d3c23b0a0c58ba5077
amit-upadhyay-it <youarefreak1@gmail.com>
2017-09-18 23:53:36 +0530

    removed dup

--- strings/java/ProblemTwo.java
+++ strings/java/ProblemTwo.java
@@ -1,31 +1,29 @@
-        public class ProblemTwo {
+public class ProblemTwo {
 
-        private void removeDuplicates(char []cStr)
+private void removeDuplicates(char []cStr)
+{
+    int hash[] = new int[256];// all values are initialized to 0
+    int j = 0;// for filling up the cStr
+    for (int i = 0; i < cStr.length; ++i)
+    {
+        if (hash[cStr[i]] == 0)
         {
-        int hash[] = new int[256];// all values are initialized to 0
-        int j = 0;// for filling up the cStr
-        for (int i = 0; i < cStr.length; ++i)
-        {
-            if (hash[cStr[i]] == 0)
-            {
-                hash[cStr[i]] = 1;
-                cStr[j++] = cStr[i];
-            }
+            hash[cStr[i]] = 1;
+            cStr[j++] = cStr[i];
         }
-        cStr[j] = '\0';
-        System.out.println(cStr);
-        }
+    }
+    cStr[j] = '\0';
+    System.out.println(cStr);
+}
 
-        public static void main(String[] args) {
+public static void main(String[] args) {
 
-        //StringBuilder str = new StringBuilder("gaadbbaaggaiij");
+    String str = new String("gaadbbaaggaiij");
+    char []cStr = str.toCharArray();
 
-        String str = new String("gaadbbaaggaiij");
-        char []cStr = str.toCharArray();
+    new ProblemTwo().removeDuplicates(cStr);
 
-        new ProblemTwo().removeDuplicates(cStr);
+    }
 
-        }
-
-        }
+}
 
