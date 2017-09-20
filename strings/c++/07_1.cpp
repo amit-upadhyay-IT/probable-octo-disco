4c80c1e9b171d8ca434ddb2fba0fcaceb4d471bd
amit-upadhyay-it <youarefreak1@gmail.com>
2017-09-20 12:20:32 +0530

    first non-repeating

---
+++ strings/c++/07_1.cpp
@@ -0,0 +1,35 @@
+/*
+ * find the first non-repeating characters in given string
+ * */
+#include<iostream>
+#include<cstring>
+
+char getFirstNonRepeating(char *str, int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        int flag = true;
+        char c = str[i];
+        for (int j = 0; j < n; ++j)
+        {
+            if (i != j)
+                if (c == str[j])
+                    flag = false;
+        }
+        if (flag)
+            return c;
+    }
+    return '\0';
+}
+
+int main()
+{
+    char *str = new char[1000];
+    std::cin.getline(str, 1000);// 1000 is the size of input buffer created
+    char ch = getFirstNonRepeating(str, strlen(str));
+    if (ch != '\0')
+        std::cout<<ch<<std::endl;
+    else
+        std::cout<<"Each character is repeating\n";
+    return 0;
+}
