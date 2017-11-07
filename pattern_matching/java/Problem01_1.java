dcb616f2e4efd0fa90f0ee1ad51aba90701081c1
amit-upadhyay-it <youarefreak1@gmail.com>
2017-11-07 19:48:47 +0530

    dfa approach

--- pattern_matching/java/Problem01_1.java
+++ pattern_matching/java/Problem01_1.java
@@ -1,28 +1,26 @@
-package sols;
-
 public class Problem01_1 {
 
-	public static void main(String[] args) {
-		
-		String str1 = "abbabbaaabba";
-		
-		System.out.println("The number occurrence(s) of 'abba' in 'abbabbaaabba' is "+ getOccurrence(str1));
-	}
-	
-	private static int getOccurrence(String str1)
-	{
-		int cnt = 0;
-		DFA obj = new DFA();
-		
-		for (int i = 0; i < str1.length(); ++i)
-		{
-			obj.setNextState(str1.charAt(i));
-			
-			if (obj.currentState == STATES.FIFTH)
-				cnt++;
-		}
-		return cnt;
-	}
+    public static void main(String[] args) {
+
+        String str1 = "abbabbaaabba";
+
+        System.out.println("The number occurrence(s) of 'abba' in 'abbabbaaabba' is "+ getOccurrence(str1));
+    }
+
+    private static int getOccurrence(String str1)
+    {
+        int cnt = 0;
+        DFA obj = new DFA();
+
+        for (int i = 0; i < str1.length(); ++i)
+        {
+            obj.setNextState(str1.charAt(i));
+
+            if (obj.currentState == STATES.FIFTH)
+                cnt++;
+        }
+        return cnt;
+    }
 
 }
 
@@ -31,56 +29,54 @@ enum STATES {FIRST, SECOND, THIRD, FOURTH, FIFTH};
 // a static DFA for accepting input sequence 'abba'
 class DFA
 {
-	STATES currentState = STATES.FIRST, nextState;
-	char input;
-	
-	public void setNextState(char input)
-	{
-		if (input == 'a')
-		{
-			switch (currentState)
-			{
-				case FIRST:
-					currentState = STATES.SECOND;
-					break;
-				case SECOND:
-					currentState = STATES.SECOND;
-					break;
-				case THIRD:
-					currentState = STATES.SECOND;
-					break;
-				case FOURTH:
-					currentState = STATES.FIFTH;
-					break;
-				case FIFTH:
-					currentState = STATES.SECOND;
-					break;
-				
-			}
-		}
-		else if (input == 'b')
-		{
-			switch (currentState)
-			{
-				case FIRST:
-					currentState = STATES.FIRST;
-					break;
-				case SECOND:
-					currentState = STATES.THIRD;
-					break;
-				case THIRD:
-					currentState = STATES.FOURTH;
-					break;
-				case FOURTH:
-					currentState = STATES.FIRST;
-					break;
-				case FIFTH:
-					currentState = STATES.THIRD;
-					break;
-				
-			}
-		}
-	}
+    STATES currentState = STATES.FIRST, nextState;
+    char input;
+
+    public void setNextState(char input)
+    {
+        if (input == 'a')
+        {
+            switch (currentState)
+            {
+                case FIRST:
+                    currentState = STATES.SECOND;
+                    break;
+                case SECOND:
+                    currentState = STATES.SECOND;
+                    break;
+                case THIRD:
+                    currentState = STATES.SECOND;
+                    break;
+                case FOURTH:
+                    currentState = STATES.FIFTH;
+                    break;
+                case FIFTH:
+                    currentState = STATES.SECOND;
+                    break;
+            }
+        }
+        else if (input == 'b')
+        {
+            switch (currentState)
+            {
+                case FIRST:
+                    currentState = STATES.FIRST;
+                    break;
+                case SECOND:
+                    currentState = STATES.THIRD;
+                    break;
+                case THIRD:
+                    currentState = STATES.FOURTH;
+                    break;
+                case FOURTH:
+                    currentState = STATES.FIRST;
+                    break;
+                case FIFTH:
+                    currentState = STATES.THIRD;
+                    break;
+            }
+        }
+    }
 }
 
 // Time for getting number of occurrence is O(n)
