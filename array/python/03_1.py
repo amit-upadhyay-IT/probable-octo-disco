457dcfe5ee12d397864c3b23f825dd562b0a72f6
amit-upadhyay-it <youarefreak1@gmail.com>
2017-12-01 22:48:54 +0530

    mva

---
+++ array/python/03_1.py
@@ -0,0 +1,32 @@
+def getMajorityCandidate(li):
+    majority_candidate = li[0]
+    candidate_cnt = 1
+
+    for i in range(1, len(li)-1):
+        if majority_candidate == li[i]:
+            candidate_cnt += 1
+        else:
+            if candidate_cnt == 0:
+                majority_candidate = li[i]
+                candidate_cnt = 1
+            else:
+                candidate_cnt -= 1
+
+    return majority_candidate
+
+
+def isMaj(li, candidate):
+    cnt = 0
+    for i in li:
+        if i == candidate:
+            cnt += 1
+
+    return cnt >= len(li)/2
+
+
+li = [5, 2, 3, 78, 5, 5, 5, 5, 6, 5, 4, 5, 5, 5]
+
+if isMaj(li, getMajorityCandidate(li)):
+    print getMajorityCandidate(li), 'is the majority element'
+else:
+    print 'no majority element'
