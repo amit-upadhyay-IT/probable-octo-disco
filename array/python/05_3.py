35e5c33a4ecad3e9b408a27a9aa4b428ef793205
amit-upadhyay-it <youarefreak1@gmail.com>
2017-12-02 00:39:42 +0530

    doesn't work

--- array/python/05_3.py
+++ array/python/05_3.py
@@ -2,9 +2,13 @@
 
 li = [1, 2, 3, 5, 4, 6, 8, 1, 2, 3, 5, 6, 7, 8]
 
-def getOddOccurred(li):
-    res = li[0]
-    for i in range(0, len(li)):
-        res ^= li[i]
 
-    return li
+def getOddOccurred(li):
+    res = 0
+    for i in li:
+        res = res ^ i
+
+    return res
+
+
+print "%d" % getOddOccurred(li)
