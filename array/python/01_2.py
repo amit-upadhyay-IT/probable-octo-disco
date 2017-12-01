7f6ebb961e23c5f09e275d50ae093a8f2939beaa
amit-upadhyay-it <youarefreak1@gmail.com>
2017-12-01 18:13:09 +0530

    complete

--- array/python/01_2.py
+++ array/python/01_2.py
@@ -1,19 +1,24 @@
 def bSearch(li, key, fI, lI):
-    if fI >= lI:
+    if fI > lI:
         return -1
     mid = (fI + lI) / 2
     if li[mid] == key:
         return mid  # element returned
     elif li[mid] > key:
-        bSearch(li, key, fI, mid-1)
+        return bSearch(li, key, fI, mid-1)
     else:
-        bSearch(li, key, mid+1, lI)
+        return bSearch(li, key, mid+1, lI)
 
 
 li = [10, 65, 103, 111, 150, 263, 336]
 
-retVal = bSearch(li, 111, 0, len(li)-1)
+
+def getPair(li, x):
+    for i in range(0, len(li)-1, 1):
+        remVal = x - li[i]
+        retVal = bSearch(li, remVal, i+1, len(li)-1)
+        if retVal != -1:
+            print '(', li[retVal], ',', li[i], ')'
 
 
-if retVal != -1:
-    print li[retVal], 'found in', li
+getPair(li, 214)
