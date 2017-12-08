bcd81811c968503056b0217e6f5f8275c1beb0b0
amit-upadhyay-it <youarefreak1@gmail.com>
2017-12-08 20:54:13 +0530

    wrong outptu

---
+++ heap/c/build_max_heap.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Heap
+{
+    int heapSize;
+    int *arr;
+};
+
+void max_heapify(struct Heap *h, int i)
+{
+    int l = 2*i;
+    int r = 2*i + 1;
+    int largest = -1;
+
+    if (l <= h->heapSize && h->arr[l] > h->arr[i])
+        largest = l;
+    else
+        largest = i;
+
+    if (r <= h->heapSize && h->arr[r] > h->arr[i])
+        largest = r;
+
+    if (largest != i) // need for heapify
+    {
+        int t = h->arr[i];
+        h->arr[i] = h->arr[largest];
+        h->arr[largest] = t;
+
+        max_heapify(h, largest);
+    }
+}
+
+void buildMaxHeap(struct Heap *h)
+{
+    int i;
+    for (i = 0; i < h->heapSize; ++i)
+        max_heapify(h, i);
+}
+
+void printHeap(struct Heap *h)
+{
+    int i;
+    printf("\n");
+    for (i = 0; i < h->heapSize; ++i)
+        printf("%d ", h->arr[i]);
+    printf("\n");
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    struct Heap *h;
+    h->arr = malloc(sizeof(int)*n);
+    h->heapSize = n;
+
+    int i;
+    for (i = 0; i < n; ++i)
+        scanf("%d", &h->arr[i]);
+
+    buildMaxHeap(h);
+
+    printf("The max heap is ");
+    printHeap(h);
+
+    return 0;
+}
