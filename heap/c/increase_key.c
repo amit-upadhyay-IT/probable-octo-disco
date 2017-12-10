627af802d0831538e3a734a6179c52594e3c4e99
amit-upadhyay-it <youarefreak1@gmail.com>
2017-12-10 11:31:05 +0530

    success

--- heap/c/increase_key.c
+++ heap/c/increase_key.c
@@ -11,6 +11,72 @@ struct Heap
     int *arr;
 };
 
+void max_heapify(struct Heap *h, int i)
+{
+    int l = 2*i;
+    int r = 2*i + 1;
+    int largest = -1;
+
+    if (l <= h->heapSize && h->arr[l] > h->arr[i])
+        largest = l;
+    else
+        largest = i;
+
+    if (r <= h->heapSize && h->arr[r] > h->arr[largest])
+        largest = r;
+
+    if (largest != i) // need for heapify
+    {
+        int t = h->arr[i];
+        h->arr[i] = h->arr[largest];
+        h->arr[largest] = t;
+
+        max_heapify(h, largest);
+    }
+}
+
+void buildMaxHeap(struct Heap *h)
+{
+    int i;
+    for (i = h->heapSize/2; i >= 1; --i)
+        max_heapify(h, i);
+}
+
+void increase_key(struct Heap* h, int index, int key)
+{
+    if (key <= h->arr[index])
+    {
+        printf("\nError\n");
+        return;
+    }
+
+    h->arr[index] = key;
+
+    // as we have increased the value of node, so property of max heap will still be maintained, so just see if parent node has been disturbed or not
+
+    while (index > 1 && h->arr[index/2] < h->arr[index])
+    {
+        // swap parent and child
+        int temp = h->arr[index];
+        h->arr[index] = h->arr[index/2];
+        h->arr[index/2] = temp;
+
+        index /= 2;
+    }
+}
+
+void printHeap(struct Heap *h)
+{
+    int i;
+    printf("\n");
+    for (i = 1; i <= h->heapSize; ++i)
+    {
+        printf("%d ", h->arr[i]);
+    }
+    printf("\n");
+}
+
+
 int main()
 {
     int i;
@@ -21,4 +87,17 @@ int main()
     printf("\nEnter heap elements \n");
     for (i = 1; i <= h->heapSize; ++i)
         scanf("%d", &h->arr[i]);
+
+    buildMaxHeap(h);
+
+    int ind, val;
+    printf("\nEnter the index and the value with which you wanna replace the key\n");
+    scanf("%d%d", &ind,&val);
+
+    printf("\nThe heap before increasing the key\n");
+    printHeap(h);
+
+    increase_key(h, ind, val);
+    printf("\nThe heap after increasing the key\n");
+    printHeap(h);
 }
