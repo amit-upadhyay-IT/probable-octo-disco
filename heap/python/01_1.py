8408dc8fff9b0f5faca12e4947d020e4b83acaa4
amit-upadhyay-it <youarefreak1@gmail.com>
2017-12-11 14:06:53 +0530

    maxinhe

--- heap/python/01_1.py
+++ heap/python/01_1.py
@@ -1,7 +1,20 @@
-import maxheap
+import minheap
+import sys
+
+#  iterating from the beginning of leaf to end of leaf
+
+
+def getMax(minHeap):
+    large = -sys.maxint-1
+    for i in range(minHeap.heapSize/2+1, minHeap.heapSize+1, 1):
+        if minHeap.heapList[i] > large:
+            large = minHeap.heapList[i]
+
+    return large
+
 
 if __name__ == '__main__':
-    sampleList = ['empty', 1, 5, 6, 8, 12, 14, 16]
-    maxHeap = maxheap.MaxHeap(len(sampleList)-1, sampleList)
-    maxHeap.build_maxheap()
-    maxHeap.print_heap()
+    samplelist = ['empty', 65, 13, 17, 18, 48, 79, 68]
+    minHeap = minheap.MinHeap(len(samplelist)-1, samplelist)
+    minHeap.build_minheap()
+    print 'max is', getMax(minHeap)
--- heap/python/minheaptest.py
+++
@@ -1,7 +0,0 @@
-import minheap
-
-if __name__ == '__main__':
-    samplelist = ['empty', 65, 13, 17, 18, 48, 79, 68]
-    minHeap = minheap.MinHeap(len(samplelist)-1, samplelist)
-    minHeap.build_minheap()
-    minHeap.print_heap()
