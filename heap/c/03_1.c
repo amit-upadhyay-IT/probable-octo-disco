16c2ee4ba0c991b640210abf8806529f9e2fc2c4
amit-upadhyay-it <youarefreak1@gmail.com>
2017-12-12 23:57:59 +0530

    optimalway

---
+++ heap/c/03_1.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+  
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+  
+void minHeapify(int arr[], int size, int index)
+{
+    int left = 2*index+1;
+    int right = 2*index+2;
+    int smallest = index;
+    if(left<size && arr[left]<arr[smallest])
+        smallest = left;
+    if(right<size && arr[right]<arr[smallest])
+        smallest = right;
+    if(smallest!=index)
+    {
+        swap(&arr[index], &arr[smallest]);
+        minHeapify(arr, size, smallest);
+    }     
+}
+  
+  
+void buildMinHeap(int *arr, int size) {
+    int index;
+    for(index = size/2; index >= 0; index--)
+        minHeapify(arr, size, index);         
+}  
+
+void printKElements(int *minHeap, int k)
+{
+    int index;
+    for(index = 0; index < k; index++)
+    	printf("%d\t", minHeap[index]);
+}
+  
+void kLargestElements(int *arr, int size, int k)
+{
+    int index;
+    buildMinHeap(arr, k);
+    for(index = k; index < size; index++)
+    {
+        if(arr[index] > arr[0])
+        {
+            arr[0] = arr[index];
+            minHeapify(arr, k, 0);
+        }
+    }
+    printKElements(arr, k);
+}
+  
+  
+int main() {
+    int *arr, size, k, index;
+    printf("Enter size of the heap\n");
+    scanf("%d", &size);
+    //allocate memory
+    arr = (int *)malloc(sizeof(int) * size);
+    printf("Enter elements to heap\n");
+    for(index = 0; index < size; index++)
+        scanf("%d", &arr[index]);
+    printf("Enter value of k\n");
+    scanf("%d", &k);
+    kLargestElements(arr, size, k);
+    return 0;
+}
