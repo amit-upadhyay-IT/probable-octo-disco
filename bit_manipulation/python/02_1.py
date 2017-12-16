28e244e3fe137923c97b76d4247a595212ca805e
amit-upadhyay-it <youarefreak1@gmail.com>
2017-12-16 13:32:15 +0530

    setit

---
+++ bit_manipulation/python/02_1.py
@@ -0,0 +1,11 @@
+def setBit(num, k):
+    return num | (1 << k)
+
+
+inp = raw_input('enter the number and kth bit separated by commas:')
+
+nums = inp.split(',')
+
+print 'the number after setting kth bit is', setBit(int(nums[0]), int(nums[1]))
+
+# time complexity = O(logn)
