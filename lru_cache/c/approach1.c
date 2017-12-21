e61a4f52ffd878e553343916077e7793ef7e805f
amit-upadhyay-it <youarefreak1@gmail.com>
2017-12-21 10:40:45 +0530

    works great

--- lru_cache/c/approach1.c
+++ lru_cache/c/approach1.c
@@ -2,18 +2,6 @@
 #include<stdlib.h>
 #include<string.h>
 
-char* getAuxArray(char* str, int c_size)
-{
-    char *aux = malloc(sizeof(char)*c_size);
-    int i;
-
-    /* in the aux array we should insert from the end because, we need to make sure
-    that the least recently used element should be at the end and most recently used element should be at front*/
-    for (i = 0; i < c_size; ++i)
-        aux[c_size-1-i] = str[i];
-
-    return aux;
-}
 
 void printAux(char *aux, int c_size)
 {
@@ -21,7 +9,10 @@ void printAux(char *aux, int c_size)
     int i;
     for (i = 0; i < c_size; ++i)
     {
-        printf("%d\n", aux[i]);
+        if (aux[i] >= 48 && aux[i] <= 58)
+            printf("%d\n", aux[i]-48);
+        else
+            printf("%d\n", aux[i]);
     }
     printf("\n");
 }
@@ -35,13 +26,39 @@ int findOutBlock(char *aux, char key, int c_size)
     return 0;
 }
 
-void replaceBlock(char* aux, char next, int c_size)
+int getFoundIndex(char *aux, int key, int c_size)
+{
+    int i;
+    for (i = 0; i < c_size; ++i)
+        if (key == aux[i])
+            return i;
+    return -1;
+}
+
+void replaceBlock(char* aux, char next, int c_size, int *faultCnt)
 {
     int isBlockAvailable = findOutBlock(aux, next, c_size);
 
     if (isBlockAvailable)
     {
-        return;
+        // here I want to put the found block on the front.
+        // so for this I would find the index of found block, then from its left
+        // I would shift right each element by 1, at last I would put found block at 0 index
+        int foundIndex = getFoundIndex(aux, next, c_size);
+        if (foundIndex != -1)
+        {
+            int i;
+            for (i = foundIndex-1; i >= 0; --i)
+            {
+                aux[i+1] = aux[i];
+            }
+            aux[0] = next;
+        }
+        else
+        {
+            printf("\nSomething went wrong!\n");
+        }
+        return ;
     }
     // since the block is not available so I need to remove last element (least recently used),
     // shift each element to right by one and insert the new element.
@@ -51,6 +68,14 @@ void replaceBlock(char* aux, char next, int c_size)
         aux[i+1] = aux[i];
     }
     aux[0] = next;
+    (*faultCnt)++;
+}
+
+void initializeAux(char* aux, int c_size)
+{
+    int i;
+    for (i = 0; i < c_size; ++i)
+        aux[i] = -1;
 }
 
 int main()
@@ -69,15 +94,18 @@ int main()
 
     if (c_size < len)
     {
-        char *aux = getAuxArray(str, c_size);
+        int faultCnt = 0;
+        char *aux = malloc(c_size);
+        initializeAux(aux, c_size);
 
         int i;
-        for (i = c_size; i < len; ++i)
+        for (i = 0; i < len; ++i)
         {
-            replaceBlock(aux, str[i], c_size);
+            replaceBlock(aux, str[i], c_size, &faultCnt);
+            printAux(aux, c_size);
         }
 
-        printAux(aux, c_size);
+        printf("\npage fault count = %d\n", faultCnt);
     }
     return 0;
 }
