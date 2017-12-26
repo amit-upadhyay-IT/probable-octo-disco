51cf91bb7a0c24c30cced31c04c719d570fb5a7d
amit-upadhyay-it <youarefreak1@gmail.com>
2017-12-26 22:27:02 +0530

    inorder udpate

--- trees/python/inorder.py
+++ trees/python/inorder.py
@@ -2,6 +2,16 @@ import bst
 
 
 # here I am using list as stack
+
+'''
+the idea here is about printing the left most node first, i.e. we can store the
+root.left node in the stack until we are finding the root.left in not None.Then
+the last node which got inserted needs to be printed, so we just need a pop for
+that. Next we can print the parent of the poped element(as that would be on top
+). Then we should insert the right ones
+'''
+
+
 def iterative_inorder(root):
     if not root:
         return root
