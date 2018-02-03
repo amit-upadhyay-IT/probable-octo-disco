a11128dcecf6792478bb30d2c491564ea388d6ad
amit-upadhyay-it <youarefreak1@gmail.com>
2018-02-03 16:00:44 +0530

    countpeopleincolumn

---
+++ recursion/python/tests/count_people_in_colums.py
@@ -0,0 +1,22 @@
+import random
+
+
+# count consequitive ones
+def count_it(li, ind):
+    # base case: when we found that next number if zero
+    if li[ind] == 0:
+        return 0
+    else:
+        val = count_it(li, ind+1)
+        val = val + 1
+        return val
+
+
+if __name__ == '__main__':
+    n = random.randint(3, 66)
+    li = [0]*2*n
+    li[0:n] = [1]*n  # 1 represents the people in the row, among 2n capacities
+    if n == count_it(li, 0):
+        print 'success'
+    else:
+        print 'its not possible'
