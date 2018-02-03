b06b36c3c9c86d809a459fc64a9be98dc0d05e39
amit-upadhyay-it <youarefreak1@gmail.com>
2018-02-03 22:05:46 +0530

    update

--- recursion/python/tests/decimal_to_binary.py
+++ recursion/python/tests/decimal_to_binary.py
@@ -1,14 +1,18 @@
 def decimal_to_binary(n):
-    if n == 1:
-        print 1,
+    # check for negative numbers
+    if n < 0:
+        print '-',
+        # since, I am recusive, so call me
+        decimal_to_binary(-n)
+    elif n < 2:
+        # coz, these number has the same binary representation as the decimal
+        print n,
     else:
-        if n & 1:
-            decimal_to_binary(n/2)
-            print 1,  # backtracking here as we print in reverse order
-        else:
-            decimal_to_binary(n/2)
-            print 0,  # backtracking
+        decimal_to_binary(n/2)
+        print n % 2,  # backtracking here as we print in reverse order
 
 
 if __name__ == '__main__':
     decimal_to_binary(43)
+    print
+    decimal_to_binary(-43)
