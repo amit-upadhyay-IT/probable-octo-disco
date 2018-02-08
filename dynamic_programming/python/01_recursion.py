08bd47934b0c2eaedee01e9e676e5f2a34646bd5
amit-upadhyay-it <youarefreak1@gmail.com>
2018-02-08 23:25:00 +0530

    update

--- dynamic_programming/python/01_recursion.py
+++ dynamic_programming/python/01_recursion.py
@@ -3,25 +3,34 @@ recursive program for lis
 '''
 
 
+# a helper function where actual recursion takes place to find the
+# lis of arr with a specified length, i.e. the function below will just
+# find the possible maximum lis with the specified length of array
 def lis_helper(arr, last):
     # base case, i.e. when the last index goes down to 1
     # array with one element will always have lis as 1
     if last == 0:
         return 1
     else:
-        r = 1
-        # form n-1 branches
+        # using a var to get the maximum among the possible choises
+        maxi = 1
+        # forming n branches and getting the maximum among them
         for i in xrange(last):
+            # we need to recusively call this function only if we find
+            # arr[n] > arr[n-1], otherwise we know that lis for that case
+            # will be 1 (and that's why maxi is initialized with 1)
             if arr[i] < arr[last]:
+                # adding one because the new element can be included in lis
                 res = lis_helper(arr, last-1) + 1
-                if res > r:
-                    r = res
-        return r
+                if res > maxi:
+                    maxi = res
+        return maxi
 
 
+# returns the length of lis
 def lis(arr):
-    res = 0
     maxi = 0
+    # getting all the possible lis and chosing maximum among them
     for i in xrange(len(arr)):
         res = lis_helper(arr, i)
         if res > maxi:
@@ -32,3 +41,6 @@ def lis(arr):
 if __name__ == '__main__':
     arr = [2, 3, 1, 5, 12, 10, 11]
     print lis(arr)
+
+# time = O(n!), use dp to optimize this problem, however I solved
+# this used recursion for better understanding of recursions
