85eb1e6c86dbd920289778ca196c2f6d49106882
amit-upadhyay-it <youarefreak1@gmail.com>
2018-03-20 00:06:47 +0530

    update

--- recursion/python/power.py
+++ recursion/python/power.py
@@ -1,14 +1,18 @@
 # calculates x^y
+# time = O(n), space = O(n)
+
+
 def power2(x, y):
     if y == 0:
         return 1
     else:
-        ret = power(x, y-1)
+        ret = power2(x, y-1)
         ret = ret * x  # backtracking
         return ret  # backtracking
         # return x * power(x, y-1)
 
 
+# time = O(n)
 def power(x, y):
     if y == 0:
         return 1
@@ -18,5 +22,17 @@ def power(x, y):
         return power(x, y/2) * power(x, y/2)
 
 
+# time = O(log2(n))
+def power_optimized(x, y):
+    if y == 0:
+        return 1
+    p = power_optimized(x, y/2)
+    if y & 1:  # i.e. odd
+        return x * p * p
+    else:
+        return p * p
+
+
 if __name__ == '__main__':
     print power(2, 3)
+    print power_optimized(6, 7)
