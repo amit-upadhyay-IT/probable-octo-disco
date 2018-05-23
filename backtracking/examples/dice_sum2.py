c8caf64ae70be16adb2b9bdab947a1a730e76875
amit-upadhyay-it <youarefreak1@gmail.com>
2018-05-23 14:21:25 +0530

    refactored

--- backtracking/examples/dice_sum2.py
+++ backtracking/examples/dice_sum2.py
@@ -12,21 +12,5 @@ def dice_roll2(n, lis, desired_sum, sum_so_far):
                 lis.pop()  # removing from last
 
 
-cnt = 0
-
-
-def dice_roll3(lis, desired_sum, sum_so_far):
-    if desired_sum == sum_so_far:
-        global cnt
-        cnt += 1
-        # print lis
-    else:
-        for i in xrange(1, 7):
-            if sum_so_far < desired_sum:
-                lis.append(i)
-                dice_roll3(lis, desired_sum, sum_so_far + i)
-                lis.pop()  # removing from last
-
-
 if __name__ == '__main__':
     dice_roll2(2, [], 8, 0)
